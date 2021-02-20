d3e090489652b3fb70143f3e0fbe56f5d35370fe
Alexander <al.tulin@gmail.com>
2021-02-20 12:29:00 +0800

    svg

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,26 @@
-const projectFolder = `dist`;
-const sourceFolder = `src`;
+const projectFolder = `name_project`; //Папка продакшн
+const sourceFolder = `_src`;  // Папка разработки
+
+// js 
+const sourceJs = `${sourceFolder}/js/app.js`; // файл для разработки Пользовательские скрипты
+const projectJs = `script.js`; // файл в продакшн
+const projectJsMin = `script.min.js`; // файл в продакшн минифицированный
+
+// установленные библиотеки js
+const jquery_js = `node_modules/jquery/dist/jquery.min.js`;
+
+// css
+const sourceCss = `${sourceFolder}/sass/style.sass` // файл для разработки Пользовательские стили
+const projectCss = `style.css`; // файл в продакшн
+const projectCssMin = `style.min.css`; // файл в продакшн минифицированный
+
+// установленные библиотеки css
+const normalize_css = `node_modules/normalize.css/normalize.css`;
+
+// img
+const sourceImg = `${sourceFolder}/_img/**/*`;//папка для разработки
+const projectImg = `${sourceFolder}/img/**/*`;// папка в продакшн
+
 
 const path = {
   build: {
@@ -10,15 +31,15 @@ const path = {
     fonts: `${projectFolder}/fonts`
   },
   src: {
-    html: `${sourceFolder}/**/*.html`, //
-    css: `${sourceFolder}/css/app.min.css`, //
+    html: `${sourceFolder}/*.html`, //
+    css: `${sourceFolder}/css/style.min.css`, //
     min_js: `${sourceFolder}/js/script.min.js`, //
     js: `${sourceFolder}/js/script.js`, //
-    img: `${sourceFolder}/img/dest/**/*`, //
+    img: `${sourceFolder}/img/**/*`, //
     fonts: `${sourceFolder}/fonts/**/*`, //
     pug: `${sourceFolder}/pug/pages/*.pug`,//
-    sass: `${sourceFolder}/sass/style.sass`,
-    libs_js: `${sourceFolder}/js/libs/**/*.js`//
+    libs_js: `${sourceFolder}/js/libs/**/*.js`,//
+    libs_css: `${sourceFolder}/css/libs/**/*`//
   },
   watch: {
     // html: `${sourceFolder}/**/*.html`,
@@ -52,61 +73,74 @@ function browsersync() {
     open: false,
     port: 3000
   })
-}
+};
 
 function scripts() {
   return src([ // Берём файлы из источников
-    'node_modules/jquery/dist/jquery.min.js', // Пример подключения библиотеки
-    // 'node_modules/mmenu-light/dist/mmenu-light.js', // Пример подключения библиотеки
-    // 'src/js/libs/*',
-    path.src.libs_js,
-    `${sourceFolder}/js/app.js`, // Пользовательские скрипты, использующие библиотеку, должны быть подключены в конце
+    jquery_js,
+    path.src.libs_js, // библиотеки из папки libs
+    sourceJs, // Пользовательские скрипты, использующие библиотеку, должны быть подключены в конце
   ])
     .pipe(sourcemaps.init())
-    .pipe(concat('script.js')) // Конкатенируем в один файл
-    .pipe(dest(`${sourceFolder}/js`))
-    .pipe(concat('script.min.js'))
+    .pipe(concat(projectJs)) // Конкатенируем в один файл
+    // .pipe(sourcemaps.write()) //добавляем карту
+    .pipe(dest(`${sourceFolder}/js`)) // Выгружаем готовый файл не мин в папку назначения
+    .pipe(concat(projectJsMin))
     .pipe(uglify()) // Сжимаем JavaScript
     // .pipe(sourcemaps.write()) //добавляем карту
-    .pipe(dest(`${sourceFolder}/js`)) // Выгружаем готовый файл в папку назначения
+    .pipe(dest(`${sourceFolder}/js`)) // Выгружаем готовый файл мин в папку назначения
     .pipe(browserSync.stream()) // Триггерим Browsersync для обновления страницы
-}
+};
 
 function styles() {
-  return src([
-    'node_modules/normalize.css/normalize.css',
-    // 'node_modules/mmenu-light/dist/mmenu-light.css',
-    'src/css/libs/*',
-    'src/sass/style.sass'
+  return src([// Выбираем источникИ
+    normalize_css,
+    path.src.libs_css,
+    sourceCss
 
-  ]) // Выбираем источник: "app/sass/main.sass" или "app/less/main.less"
+  ])
     .pipe(sourcemaps.init())
     .pipe(sass())
-    .pipe(concat('app.min.css')) // Конкатенируем в файл app.min.js
+    .pipe(concat(projectCssMin)) // Конкатенируем в файл 
     .pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true })) // Создадим префиксы с помощью Autoprefixer
     .pipe(cleancss({ level: { 1: { specialComments: 0 } }/* , format: 'beautify' */ })) // Минифицируем стили
     .pipe(sourcemaps.write(".")) //добавляем карту
-    .pipe(dest('src/css/')) // Выгрузим результат в папку "app/css/"
+    .pipe(dest(`${sourceFolder}/css/`)) // Выгрузим результат в папку 
     .pipe(browserSync.stream()) // Сделаем инъекцию в браузер
 }
 
 function images() {
-  return src('src/img/src/**/*') // Берём все изображения из папки источника
-    .pipe(newer('src/img/dest/')) // Проверяем, было ли изменено (сжато) изображение ранее
+  return src(sourceImg) // Берём все изображения из папки источника
+    .pipe(newer(projectImg)) // Проверяем, было ли изменено (сжато) изображение ранее
     .pipe(imagemin([
       imagemin.optipng({ optimizationLevel: 3 }),
       imagemin.mozjpeg({ quality: 75, progressive: true }),
-      imagemin.svgo()
+      imagemin.svgo({
+        plugins: [
+          { removeViewBox: true },
+          { cleanupIDs: false }
+        ]
+      })
     ])) // Сжимаем и оптимизируем изображеня
-    .pipe(dest('src/img/dest/')) // Выгружаем оптимизированные изображения в папку назначения
+    .pipe(dest(`${sourceFolder}/img`)) // Выгружаем оптимизированные изображения в папку назначения
+
+    .pipe(imagemin([
+      imagemin.svgo({
+        plugins: [
+          { removeViewBox: true },
+          { cleanupIDs: false }
+        ]
+      })
+    ])) // оптимизируем svg
+    .pipe(dest(`${sourceFolder}/img/svg`)) // Выгружаем оптимизированные изображения в папку назначения
 }
 
-function cleanimg() {
-  return del('src/images/dest/**/*', { force: true }) // Удаляем всё содержимое папки "app/images/dest/"
-}
+// function cleanimg() {
+//   return del('src/images/dest/**/*', { force: true }) // Удаляем всё содержимое папки "app/images/dest/"
+// }
 
-function cleandist() {
-  return del(`${projectFolder}/**/*`, { force: true }) // Удаляем всё содержимое папки "dist/"
+function cleanImg() {
+  return del(`projectImg`, { force: true }) // Удаляем всё содержимое папки "dist/"
 }
 
 function buildcopy() {
@@ -123,19 +157,24 @@ function buildcopy() {
 
 
 function startwatch() {
-  // Выбираем все файлы JS в проекте, а затем исключим с суффиксом .min.js
-  watch(['src/js/*.js', '!src/js/*.min.js'], scripts);
-  watch(['src/sass/**/*.sass', '!src/sass/style.sass'], styles);
-  // watch('src/*.html').on('change', browserSync.reload);
-  // watch('src/pug/**/*.pug').on('change', browserSync.reload);
-  watch(['src/pug/**/*.pug'], transformPug);
-  watch('src/img/src/**/*', images);
+
+  watch([
+    `${sourceFolder}/js/*.js`,
+    `!${sourceFolder}/js/${projectJs}`,
+    `!${sourceFolder}/js/${projectJsMin}`
+  ], scripts);// Выбираем все файлы JS в проекте, а затем исключим 
+
+  watch([`${sourceFolder}/sass/**/*.sass`, `!${sourceCss}`], styles);
+
+  watch([`${sourceFolder}/pug/**/*.pug`], transformPug);
+
+  watch(`${sourceFolder}/_img**/*`, images);
 }
 
 function transformPug() {
   return src(path.src.pug)
     .pipe(pug({ pretty: true })) // Преобразуем в html без минификации
-    .pipe(dest(`${sourceFolder}`))
+    .pipe(dest(`${sourceFolder}`)) // Выгружаем в папку с ф
     .pipe(browserSync.stream()) // Сделаем инъекцию в браузер
 }
 
@@ -143,9 +182,9 @@ exports.browsersync = browsersync;
 exports.scripts = scripts;
 exports.styles = styles;
 exports.images = images;
-exports.cleanimg = cleanimg;
+exports.cleanImg = cleanImg;
 exports.transformPug = transformPug;
 
 
-exports.default = parallel(styles, scripts, browsersync, startwatch);
-exports.build = series(cleandist, styles, scripts, images, buildcopy);
+exports.default = parallel(cleanImg, styles, scripts, images, browsersync, startwatch);
+exports.build = series(cleanImg, styles, scripts, images, buildcopy);
